54bb11e94e8d4263ed3f248c4350d04b91143000
guidop <guido.pederzini@gmail.com>
2013-10-27 20:03:29 +0100

    cambiato char* con std:string

--- Log/LogOutputDebugString.cpp
+++ Log/LogOutputDebugString.cpp
@@ -5,11 +5,10 @@
 
 using namespace std;
 
-void LogOutputDebugString::LogMessage(char * message, DWORD lastError, SECURITY_STATUS retValue)
+void LogOutputDebugString::LogMessage(string && message, DWORD lastError, SECURITY_STATUS retValue)
 {
-	char  msgbuf[300];
-	string msg = message;
-	msg = msg + " LastError = %ld , SecurityStatus = 0x%x \n";
-	sprintf_s(msgbuf, msg.c_str(), lastError, retValue);
+	char  msgbuf[300];	
+	message = message + " LastError = %ld , SecurityStatus = 0x%x \n";
+	sprintf_s(msgbuf, message.c_str(), lastError, retValue);
 	OutputDebugStringA(msgbuf);
 }
--- Log/LogOutputDebugString.h
+++ Log/LogOutputDebugString.h
@@ -1,10 +1,10 @@
 #pragma once
 #include "LogApi.h"
 #include <windows.h>
-
+#include <string>
 class LOG_EXPORTS_API LogOutputDebugString
 {
 public:
-	static void LogMessage(char * message, DWORD lastError, SECURITY_STATUS retValue);
+	static void LogMessage(std::string && message, DWORD lastError, SECURITY_STATUS retValue);
 };
 
