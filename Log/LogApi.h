68b6559c33b12fff3e9e6b62f6efcb57600e10d1
rramirez-tec-mx <76135643+rramirez-tec-mx@users.noreply.github.com>
2021-10-01 20:50:46 -0500

    Delete Log directory

--- Log/LogApi.h
+++
@@ -1,5 +0,0 @@
-#ifdef LOG_EXPORTS
-#define LOG_EXPORTS_API __declspec(dllexport)
-#else
-#define LOG_EXPORTS_API __declspec(dllimport)
-#endif
--- Log/LogPrint.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "LogApi.h"
-#include <string>
-class LOG_EXPORTS_API LogPrint
-{
-public:	
-	static std::string OutMessage(char* msg, bool display, ...);
-};
-
